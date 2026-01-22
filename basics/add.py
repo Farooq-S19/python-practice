f17ebae6d6265ae044f31305615dfa0867653606
Farooq-S19 <shaikfarooq9392@gmail.com>
2026-01-22 10:02:53 +0530

    Add subtraction functionality to add.py

--- basics/add.py
+++ basics/add.py
@@ -1,4 +1,6 @@
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
 sum=num1+num2
+sub=abs(num1-num2)
 print(f"{num1}+{num2} = {sum}")
+print(f"{num1}-{num2} = {sub}")
